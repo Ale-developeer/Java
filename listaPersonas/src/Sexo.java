c56969e3f7d31cc2abeaba9f8b8a6899b8700c7c
Ale-developeer <110936649+Ale-developeer@users.noreply.github.com>
2024-03-15 20:43:38 -0300

    Delete listaPersonas directory

--- listaPersonas/src/Sexo.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Sexo extends Main{
-    public static String sexo(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("ingrese sexo: ");
-        return scanner.nextLine();
-    }
-}
